b18b755b98d68d3d9c49016ff4f9851fe47e39fc
thanh thuy <thanhthuynbk44@gmail.com>
2018-05-29 17:52:01 +0700

    finish chapter 3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
   test "should get root" do
     get '/'
     assert_response :success
