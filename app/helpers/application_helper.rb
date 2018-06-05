e0d8a26b716739c1b6fb8e05d69a8326b1602c96
thanh thuy <thanhthuynbk44@gmail.com>
2018-06-05 16:05:40 +0700

    Finsih chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title)                     # Method def, optional arg
+    base_title = I18n.t(".sub_title")  # Variable assignment
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
